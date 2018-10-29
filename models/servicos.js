c1c762c5e92d9cdd9a0c6818bfc6ce2b9a0acddf
Caio Resende <caioresende@Caios-MacBook-Pro.local>
2018-10-29 14:10:07 -0300

    fixing services and style

--- models/servicos.js
+++ models/servicos.js
@@ -9,6 +9,7 @@ Servicos.add({
 	name: { type: String, required: true, initial: true, label: 'Nome Serviço' },
 	iconCSS: { type: String, required: false, initial: true, label: 'Ícone CSS' },
 	description: { type: Types.Textarea, required: true, initial: true, label: 'Descrição Serviço' },
+	destaque: { type: Types.Boolean, initial: true, label: 'Serviço de Destaque?' }
 });
 
 //Servicos.relationship({ path: 'orcamentos', ref: 'Orcamentos', refPath: 'servicos' });
