bd001d92f104e9914cc701c372c43f05565f377e
Caio Resende <cresende@symplicity.com>
2019-02-25 23:24:24 -0300

    changing gmail service

--- routes/views/index.js
+++ routes/views/index.js
@@ -123,9 +123,7 @@ exports = module.exports = function(req, res) {
 	
 		// create reusable transporter object using the default SMTP transport
 		let transporter = nodemailer.createTransport({
-		host: 'smtp.gmail.com',
-		port: 465,
-		secure: true,
+		service: 'Gmail',
 		auth: {
 			user: "carcarebrazil@gmail.com", // generated ethereal user
 			pass: "ccbr2018" // generated ethereal password
