4268bed64eab89e7a2223b35c2de42712f1d9261
Luiz Fernando Silva do Nascimento Junior <luizfsnj@acs.com.br>
2019-05-10 11:14:48 -0300

    configuração google analytics

--- web.js
+++ web.js
@@ -16,12 +16,16 @@ keystone.init({
   'session': true,
   'auth': true,
   'user model': 'User',
-  'cookie secret': '(your secret here)'
-  
+  'cookie secret': '(your secret here)',
+  'ga property': 'process.env.GA_PROPERTY',
+  'ga domain': 'process.env.GA_DOMAIN'
 });
  
 require('./models');
  
 keystone.set('routes', require('./routes'));
+
+keystone.set('ga property', 'process.env.GA_PROPERTY');
+keystone.set('ga domain', 'process.env.GA_DOMAIN');
  
 keystone.start();
