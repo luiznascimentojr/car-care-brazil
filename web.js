8160c98963294dbcb4ff7e8190e2b7c5e8bded51
Luiz Fernando Silva do Nascimento Junior <luizfsnj@acs.com.br>
2019-05-30 11:13:49 -0300

    add google analytcs

--- web.js
+++ web.js
@@ -17,15 +17,17 @@ keystone.init({
   'auth': true,
   'user model': 'User',
   'cookie secret': '(your secret here)',
-  'ga property': 'process.env.GA_PROPERTY',
-  'ga domain': 'process.env.GA_DOMAIN'
+  'ga property': process.env.GA_PROPERTY || 'alldone_site',
+  'ga domain': process.env.GA_DOMAIN || 'www.alldonecar.com.br'
 });
  
 require('./models');
  
 keystone.set('routes', require('./routes'));
 
-keystone.set('ga property', 'process.env.GA_PROPERTY');
-keystone.set('ga domain', 'process.env.GA_DOMAIN');
+keystone.set('process.env.GA_PROPERTY', 'alldone_site');
+keystone.set('process.env.GA_DOMAIN', 'www.alldonecar.com.br');
+keystone.set('ga property', 'alldone_site');
+keystone.set('ga domain', 'www.alldonecar.com.br');
  
 keystone.start();
